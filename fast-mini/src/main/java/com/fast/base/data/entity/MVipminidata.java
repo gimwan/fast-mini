d7a170e3fb1f79f1ecc92da054bacb279d39268d
gimwan <gimwan@outlook.com>
2019-04-26 19:30:37 +0800

    test

---
+++ fast-mini/src/main/java/com/fast/base/data/entity/MVipminidata.java
@@ -0,0 +1,63 @@
+package com.fast.base.data.entity;
+
+public class MVipminidata {
+    private Integer id;
+
+    private Integer miniprogramid;
+
+    private String openid;
+
+    private String unionid;
+
+    private Integer vipid;
+
+    private byte[] updatedtime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMiniprogramid() {
+        return miniprogramid;
+    }
+
+    public void setMiniprogramid(Integer miniprogramid) {
+        this.miniprogramid = miniprogramid;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getUnionid() {
+        return unionid;
+    }
+
+    public void setUnionid(String unionid) {
+        this.unionid = unionid;
+    }
+
+    public Integer getVipid() {
+        return vipid;
+    }
+
+    public void setVipid(Integer vipid) {
+        this.vipid = vipid;
+    }
+
+    public byte[] getUpdatedtime() {
+        return updatedtime;
+    }
+
+    public void setUpdatedtime(byte[] updatedtime) {
+        this.updatedtime = updatedtime;
+    }
+}
