cffc7814b80378422acafc8032aa7b2278272353
gimwan <gimwan@outlook.com>
2019-09-22 23:30:29 +0800

    2019.09.22

--- fast-mini/src/main/java/com/fast/service/ext/impl/ExtMaintServiceImpl.java
+++ fast-mini/src/main/java/com/fast/service/ext/impl/ExtMaintServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.fast.base.Result;
